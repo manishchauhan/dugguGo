ec082e3d91c438d4e13540a983eb6dc382915266
manish chauhan <negi.chauhan@gmail.com>
2023-08-06 00:39:12 +0530

    higher order function added as a util

--- src/go.mod
+++ src/go.mod
@@ -2,4 +2,4 @@ module github.com/manishchauhan/dugguGo
 
 go 1.20
 
-require github.com/gorilla/mux v1.8.0
+require golang.org/x/exp v0.0.0-20230801115018-d63ba01acd4b
--- src/main.go
+++ src/main.go
@@ -2,60 +2,17 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
 
-	"github.com/gorilla/mux"
-	"github.com/manishchauhan/dugguGo/util/tree"
+	"github.com/manishchauhan/dugguGo/util/hoc"
 )
-
-// Handler for the home route.
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the Home Page!")
-}
-
-// Handler for the about route.
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "About Page")
-}
-
-// Handler for the contact route.
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Contact Page")
-}
-func resolveTree() {
-
-	t := tree.Tree{}
-
-	t.Insert(50)
-	t.Insert(30)
-	t.Insert(20)
-	t.Insert(40)
-	t.Insert(70)
-	t.Insert(60)
-	t.Insert(80)
-
-	t.InOrderTraversal()
-}
 
 func main() {
 
-	router := mux.NewRouter()
+	values := []string{"manish", "sachin", "manish", "sachin", "deepak"}
 
-	// Define routes and their respective handlers.
-	router.HandleFunc("/", homeHandler).Methods("GET")
-	router.HandleFunc("/about", aboutHandler).Methods("GET")
-	router.HandleFunc("/contact", contactHandler).Methods("GET")
-
-	// Create a server and set the router as its handler.
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: router,
-	}
-
-	// Start the server.
-	log.Println("Server listening on http://localhost:8080")
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatalf("Server error: %v", err)
-	}
+	// Example usage of goMap with a custom function.
+	squaredValues := hoc.GoFilter[string](values, func(value string) bool {
+		return value == "deepak"
+	})
+	fmt.Println("Squared values:", squaredValues)
 }
