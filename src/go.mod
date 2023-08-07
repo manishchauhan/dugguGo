8366eb399869f8ed145bb1ec40ad6d9189729cc6
manish chauhan <negi.chauhan@gmail.com>
2023-08-07 13:04:30 +0530

    added support for mysql

--- src/go.mod
+++ src/go.mod
@@ -2,4 +2,7 @@ module github.com/manishchauhan/dugguGo
 
 go 1.20
 
-require golang.org/x/exp v0.0.0-20230801115018-d63ba01acd4b
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	golang.org/x/exp v0.0.0-20230801115018-d63ba01acd4b
+)
--- src/main.go
+++ src/main.go
@@ -2,17 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/manishchauhan/dugguGo/util/hoc"
+	"github.com/manishchauhan/dugguGo/util/mysqlDbManager"
 )
 
+func normalDb() {
+
+	dataSourceName := "root:manish@tcp(127.0.0.1:3306)/dugguGo"
+
+	// Get the DBManager instance
+	db, err := mysqlDbManager.GetInstance(dataSourceName)
+	if err != nil {
+		fmt.Println("Error creating DBManager:", err)
+		return
+	}
+	defer db.Close()
+
+	// Example query
+	rows, err := db.Query("SELECT * FROM register")
+	if err != nil {
+		fmt.Println("Error executing query:", err)
+		return
+	}
+	defer rows.Close()
+
+	// Process the rows...
+
+	var id int
+	var username string
+	var password string
+	var email string
+	for rows.Next() {
+		if err := rows.Scan(&id, &username, &password, &email); err != nil {
+			log.Println("Error scanning row:", err)
+			continue
+		}
+		fmt.Printf("Name %s\n", username)
+	}
+
+}
 func main() {
-
-	values := []string{"manish", "sachin", "manish", "sachin", "deepak"}
-
-	// Example usage of goMap with a custom function.
-	squaredValues := hoc.GoFilter[string](values, func(value string) bool {
-		return value == "deepak"
-	})
-	fmt.Println("Squared values:", squaredValues)
+	normalDb()
 }
